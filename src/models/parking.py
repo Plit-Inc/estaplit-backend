a6e35c6d7224dcc406f2450a7dc4fe958a1f8681
1-mauricio <1mmauricio@Air-de-Mauricio.local>
2023-10-01 15:28:10 -0300

    update parking model

--- src/models/parking.py
+++ src/models/parking.py
@@ -3,6 +3,9 @@ from ..connection import client
 from datetime import datetime, timedelta
 
 class Parking:
+    db = client['parking']
+    collection = db['parking']
+
     def __init__(self, free_vacancies, total_vacancies, free_reservation, total_reservation, pricing, cancellation_policy, place_id):
         self.free_vacancies = free_vacancies
         self.total_vacancies = total_vacancies
@@ -12,79 +15,78 @@ class Parking:
         self.cancellation_policy = cancellation_policy
         self.place_id = place_id
         self.reservations = []
+
+    @classmethod
+    def initialize_db_and_collection(cls, db_name='parking', collection_name='parking'):
+        cls.db = client[db_name]
+        cls.collection = cls.db[collection_name]
     
     @staticmethod
     def get_parking_by_id(id):
-        db = client['parking']
-        collection = db['parking']
-        parking = collection.find_one({'place_id': id})
-        if parking:
-            return parking
+        parking = Parking.collection.find_one({'place_id': id})
+        if parking: return parking
         return None
     
     @staticmethod
     def get_all_parking():
-        db = client['parking']
-        collection = db['parking']
-        parking = collection.find()
+        parking = Parking.collection.find()
         return parking
     
     @staticmethod
     def post_parking(parking):
         parking['reservations'] = []
-        db = client['parking']
-        collection = db['parking']
-        collection.insert_one(parking)
-        data = collection.find_one({'place_id': parking['place_id']})
-
+        Parking.collection.insert_one(parking)
+        data = Parking.collection.find_one({'place_id': parking['place_id']})
         return data
     
     @staticmethod
     def update_parking(parking):
-        db = client['parking']
-        collection = db['parking']
-        collection.update_one({'place_id': parking['place_id']}, {'$set': parking})
+        Parking.collection.update_one({'place_id': parking['place_id']}, {'$set': parking})
 
     @staticmethod
     def delete_parking(parking):
-        db = client['parking']
-        collection = db['parking']
-        collection.delete_one(parking)
+        Parking.collection.delete_one(parking)
 
     @staticmethod
     def generate_reservations(parking):
-        db = client['parking']
-        collection = db['parking']
-        reservations = []
+        week_days = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
         today = datetime.now()
-        if today.hour >= 9:
-            today = today + timedelta(days=1)
-        for i in range(7):
-            day, month = today.day, today.month
-            #get the day of the week ("0" is Segunda, "6" is Domingo)
-            day_of_week = today.weekday()
-            reservations.append({'day': day, 'month': month, 'day_of_week': day_of_week, 'reservations': []})
-            for hour in range(10, 14):
-                reservation = {'day': day, 'hour': hour, "isAvailable": True}
-                reservations[i]['reservations'].append(reservation)
-            
-            today = today + timedelta(days=1)
 
-        collection.update_one({'place_id': parking['place_id']}, {'$set': {'reservations': reservations}})
+        # Adjust today's date if it's past 9 AM
+        if today.hour >= 9:
+            today += timedelta(days=1)
+
+        reservations = []
+
+        for _ in range(7):
+            day, month = today.day, today.month
+            day_of_week = week_days[today.weekday()]
+
+            # Generate reservations for each hour from 10 to 13
+            reservations_per_day = [{'hour': hour, 'isAvailable': True} for hour in range(10, 14)]
+
+            reservations.append({
+                'day': day,
+                'month': month,
+                'day_of_week': day_of_week,
+                'reservations': reservations_per_day
+            })
+
+            today += timedelta(days=1)
+
+        # Update the reservations in the parking document
+        Parking.collection.update_one({'place_id': parking['place_id']}, {'$set': {'reservations': reservations}})
 
     @staticmethod
     def make_reservation(parking, day, month, hour):
-        db = client['parking']
-        collection = db['parking']
-        reservations = parking['reservations']
-        for i in range(len(reservations)):
-            reservation = reservations[i]
+        for reservation in parking['reservations']:
             if reservation['day'] == day and reservation['month'] == month:
-                reservation_day = reservation['reservations']
-                for j in range(len(reservation_day)):
-                    if reservation_day[j]['hour'] == hour:
-                        parking['reservations'][i]['reservations'][j]['isAvailable'] = False
-                        Parking.update_parking(parking)
-
-                        return True
+                for timeslot in reservation['reservations']:
+                    if timeslot['hour'] == hour:
+                        if timeslot['isAvailable']:
+                            timeslot['isAvailable'] = False
+                            Parking.update_parking(parking)
+                            return True  # Reservation successful
+                        else:
+                            return False  # Slot is already taken
         return None
